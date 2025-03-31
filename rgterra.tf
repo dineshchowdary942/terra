0919323da89369854998e1fbb16d9b4e18863c21
dineshchowdary942 <dineshchowdary942@gmail.com>
2025-03-31 13:16:38 +0530

    added vnet

--- rgterra.tf
+++ rgterra.tf
@@ -2,3 +2,9 @@ resource "azurerm_resource_group" "res21" {
   name = "hp"
   location = "east us"
 }
+resource "azurerm_virtual_network" "vnet22" {
+  name = "vnet11"
+  address_space = [ "10.0.0./16" ]
+  resource_group_name = azurerm_resource_group.res21.name
+  location = azurerm_resource_group.res21.location
+}
